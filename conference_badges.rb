834583a09f562fa444d8519e8b5d3b4f8d0aeb15
YanChan  Lin <yonalin1@gmail.com>
2017-10-17 21:29:27 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,14 @@
-def badge_maker("name")
-puts "Hello, my name is {name}."
-end
-speakers= [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
-def batch_badge_creator (names)
+def badge_maker(name)
+puts "Hello, my name is #{name}."
 
-end 
+end
+
+def batch_badge_creator (names)
+speakers= [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
+return badge_maker(name)
+end
+
+def assign_rooms
+ speakers= [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
+speakers.each_with_index { |name, index|}
+puts "Hello,{name}! You'll be assigned to room {index}!"
