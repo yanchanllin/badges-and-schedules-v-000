b7ad31732eae50dc00ae8f9eeea97601171a98ac
YanChan  Lin <yonalin1@gmail.com>
2017-10-17 04:24:44 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,7 @@
-# Write your code here.
+def badge_maker("name")
+puts "Hello, my name is {name}."
+end
+speakers= [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
+def batch_badge_creator (names)
+
+end 
